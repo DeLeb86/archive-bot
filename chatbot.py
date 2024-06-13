13104327e1d5e0fe7acf1a0aa54ce41a535a335f
denis <dle@verbolia.com>
2024-06-13 18:33:55 +0200

    init

---
+++ chatbot.py
@@ -0,0 +1,17 @@
+import requests
+import streamlit as st
+st.title("Archive Buddy")
+
+apiKey="fbPOjL0yn8BKWhpQVS5f9iCfPSrLgReK"
+#apiKey=st.secrets["key"]
+headers={
+    "Content-Type" : "application/json",
+    "Authorization": "Bearer " + apiKey
+    }
+form = st.form(key='my-form')
+question = form.text_input('Votre question ici.. ')
+submit_button = form.form_submit_button(label='Submit')
+if submit_button: 
+    
+    r=requests.post("https://masterclass-right-project-zoojr.westeurope.inference.ml.azure.com/score",json={"Input":question},headers=headers)
+    st.markdown(r.json()["Response"])    
